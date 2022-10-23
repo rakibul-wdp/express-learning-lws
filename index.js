63cfe51604981c075c3700e1607b3b8731df422e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-23 09:02:19 +0600

    create simple express example

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('This is home page');
+});
+
+app.post('/', (req, res) => {
+  res.send('This is home page with post request');
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
