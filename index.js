b05e68c934f83240847f95ad25e3a05b3e22030d
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-24 04:42:36 +0600

    learned mount event

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const admin = express();
 
+admin.on('mount', function (parent) {
+  console.log('Admin Mounted')
+  console.log(parent) // refers to the parent app
+})
+
 admin.get('/dashboard/hello', (req, res) => {
   console.log(admin.mountpath);
   res.send('Welcome to admin dashboard');
