26a22ea1ed723f6a4ea1f68f9cca6c6ecf57a7fc
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-24 04:47:59 +0600

    learned app.all method

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require('express');
 
 const app = express();
-const admin = express();
 
-admin.on('mount', function (parent) {
-  console.log('Admin Mounted')
-  console.log(parent) // refers to the parent app
-})
-
-admin.get('/dashboard/hello', (req, res) => {
-  console.log(admin.mountpath);
-  res.send('Welcome to admin dashboard');
-});
-
-app.get('/', (req, res) => {
+app.all('/', (req, res) => {
   res.send('welcome to application home');
 });
-
-app.use('/admin', admin);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
