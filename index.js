0cdaaeafdda341520fa83cc57d82f9bc31d28363
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-24 06:56:49 +0600

    learned req.body

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.set('view engine', 'ejs');
+const adminRoute = express.Router();
 
-app.route('/about/mission')
-  .get((req, res) => {
-    res.render('pages/about');
-  })
-  .post((req, res) => {
-    res.send('welcome to application home post');
-  })
-  .put((req, res) => {
-    res.send('welcome to application home put');
-  });
+adminRoute.get('/dashboard', (req, res) => {
+  console.log(req.query);
+  res.send('We are in Admin Dashboard');
+});
+
+app.use('/admin', adminRoute);
+
+app.get('/user/:id', (req, res) => {
+  console.log(req.query);
+  res.send('Hello world');
+});
+
+app.post('/user', (req, res) => {
+  console.log(req.body);
+  res.send('Hello world post');
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
+
+/**
+ * req.baseUrl
+ * req.originalUrl
+ * req.path
+ * req.hostname
+ * req.ip
+ * req.method
+ * req.protocol
+ * req.params
+ * req.query
+ * req.body
+ * req.cookies
+ * req.signedCookies
+ * req.secure
+ */
