533bd18077e526c2939ee52994b64ec6f8bfef12
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 07:31:54 +0600

    learned res.cookie

--- index.js
+++ index.js
@@ -5,24 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/about', (req, res) => {
-  res.format({
-    'text/plain': () => {
-      res.send('Hi');
-    },
-    'text/html': () => {
-      res.render('pages/about', {
-        name: 'Bangladesh'
-      })
-    },
-    'application/json': () => {
-      res.json({
-        message: 'About'
-      })
-    },
-    default: () => {
-      res.status(406).send('Not acceptable');
-    }
-  })
+  res.cookie('name', 'omoktomokamrai');
+  res.end();
 });
 
 app.listen(3000, () => {
