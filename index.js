4473db12572ed3669f2c0a4ce6eaff718cb95174
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 07:16:37 +0600

    learned res app and res.locals

--- index.js
+++ index.js
@@ -1,46 +1,15 @@
 const express = require('express');
-const handle = require('./handle');
 
 const app = express();
-app.use(express.json());
 
-const adminRoute = express.Router();
+app.set('view engine', 'ejs');
 
-adminRoute.get('/dashboard', (req, res) => {
-  console.log(req.secure);
-  res.send('We are in Admin Dashboard');
-});
-
-app.use('/admin', adminRoute);
-
-app.get('/user/:id', handle);
-
-app.post('/user', (req, res) => {
-  console.log(req.route);
-  res.send('Hello world post');
-});
-
-app.get('/user', (req, res) => {
-  console.log(req.route);
-  res.send('Hello world get');
+app.get('/about', (req, res) => {
+  res.render('pages/about', {
+    name: 'Bangladesh'
+  });
 });
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
-
-/**
- * req.baseUrl
- * req.originalUrl
- * req.path
- * req.hostname
- * req.ip
- * req.method
- * req.protocol
- * req.params
- * req.query
- * req.body
- * req.cookies
- * req.signedCookies
- * req.secure
- */
