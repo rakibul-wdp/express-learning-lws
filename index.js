5443893c6d58c90631786a0b252648b656d59e3a
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 07:19:04 +0600

    learned res.headerssend

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/about', (req, res) => {
+  console.log(res.headersSent);
   res.render('pages/about', {
     name: 'Bangladesh'
   });
+  console.log(res.headersSent);
 });
 
 app.listen(3000, () => {
