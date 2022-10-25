2faf3ab2d5ad707f0cae86dbad70597dd2502b0e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 07:29:42 +0600

    learned res.format

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/about', (req, res) => {
-  // res.send('About');
-  // res.end();
-  // res.json({
-  //   name: 'Bangladesh'
-  // })
-  res.sendStatus(200);
+  res.format({
+    'text/plain': () => {
+      res.send('Hi');
+    },
+    'text/html': () => {
+      res.render('pages/about', {
+        name: 'Bangladesh'
+      })
+    },
+    'application/json': () => {
+      res.json({
+        message: 'About'
+      })
+    },
+    default: () => {
+      res.status(406).send('Not acceptable');
+    }
+  })
 });
 
 app.listen(3000, () => {
