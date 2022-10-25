88f64c24cf2aa6ee89ec5777f9faa1aafb993fc9
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 15:26:04 +0600

    learned dynamic param

--- publicRouter.js
+++ publicRouter.js
@@ -2,8 +2,19 @@ const express = require('express');
 
 const publicRouter = express.Router();
 
-publicRouter.get('/', (req, res) => {
-  res.send('Home');
+publicRouter.param('user', (req, res, next, id) => {
+  req.user = id === '1' ? 'Admin' : 'Anonymous';
+  console.log('I am called once...!!!');
+  next();
+});
+
+publicRouter.get('/:user', (req, res, next) => {
+  console.log('This also matches');
+  next();
+})
+
+publicRouter.get('/:user', (req, res) => {
+  res.send(`Hello ${req.user}`);
 });
 
 publicRouter.get('/about', (req, res) => {
