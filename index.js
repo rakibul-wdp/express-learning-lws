1d83df8cb01831251d1ca4976086de63c2a08428
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 08:04:23 +0600

    learned middleware some label

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const adminRouter = express.Router();
+
 const logger = (req, res, next) => {
   console.log(`${new Date(Date.now()).toLocaleString()} - ${req.method} - ${req.originalUrl} - ${req.protocol} - ${req.ip}`);
-  res.end();
-}
+  next();
+};
 
-app.use(logger);
+adminRouter.use(logger);
+
+adminRouter.get('/dashboard', (req, res) => {
+  res.send('Dashboard');
+});
+
+app.use('/admin', adminRouter);
 
 app.get('/about', (req, res) => {
   res.send('About');
