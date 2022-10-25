2a91a63507870fcfdd1d0cc331a452f3c2c66d1b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-25 19:12:33 +0600

    handle error syncronous code

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
-const adminRouter = require('./adminRouter');
-const publicRouter = require('./publicRouter');
-
 const app = express();
 
-app.use('/admin', adminRouter);
-app.use('/', publicRouter);
+app.get('/', (req, res) => {
+  for(let i = 0; i <= 10; i++) {
+    if (i === 5) {
+      next('there was an error');
+    } else {
+      res.write('a');
+    }
+  }
+  res.end();
+});
+
+app.get('/', (req, res) => {
+  res.send(a);
+});
+
+// 404 error handler
+app.use((req, res, next) => {
+  res.send('Requested url was not found...!!!');
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    next('There wan a problem');
+  } else {
+    if (err.message) {
+      res.status(500).send(err.message);
+    } else {
+      res.status(500).send('There was an error...!!!');
+    }
+  }
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
-})
+});
