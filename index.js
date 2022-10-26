34f1dda5ca1a62c6151d0f8ec02d924b9c41a445
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-26 11:39:50 +0600

    limit some case and handle error

--- index.js
+++ index.js
@@ -6,17 +6,41 @@ const UPLOADS_FOLDER = './uploads/';
 
 // prepare the final multer upload object
 var upload = multer({
-  dest: UPLOADS_FOLDER
+  dest: UPLOADS_FOLDER,
+  limits: {
+    fileSize: 1000000, // 1MB
+  },
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === 'image/png' ||
+      file.mimetype === 'image/jpg' ||
+      file.mimetype === 'image/jpeg'
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error('only .jpg, .png or .jpeg format allowed...!!!'));
+    }
+  },
 })
 
 const app = express();
 
 // application route
-app.post('/', upload.fields([
-  {name: 'avatar', maxCount: 1},
-  {name: 'gallery', maxCount: 2},
-]), (req, res) => {
+app.post('/', upload.single('avatar'), (req, res) => {
   res.send('Hello world');
+});
+
+// default error handler
+app.use((err, req, res, next) => {
+  if (err) {
+    if (err instanceof multer.MulterError) {
+      res.status(500).send('There was an upload error');
+    } else {
+      res.status(500).send(err.message);
+    }
+  } else {
+    res.send('success');
+  }
 });
 
 app.listen(3000, () => {
