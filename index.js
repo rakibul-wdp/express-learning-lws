afaf8b78335123def469f102d617d42e468e7c8c
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-27 04:48:19 +0600

    basic setup for learning mongoose

--- index.js
+++ index.js
@@ -1,79 +1,18 @@
 const express = require('express');
-const multer = require('multer');
-const path = require('path');
 
-// File upload folder
-const UPLOADS_FOLDER = './uploads/';
-
-// define the storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, UPLOADS_FOLDER);
-  },
-  filename: (req, file, cb) => {
-    // Important File.pdf => important-file-392379372.pdf
-    const fileExt = path.extname(file.originalname);
-    const fileName = file.originalname
-      .replace(fileExt, '')
-      .toLowerCase()
-      .split(' ')
-      .join('-') + '-' + Date.now();
-    cb(null, fileName + fileExt);
-  }
-})
-
-// prepare the final multer upload object
-var upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1000000, // 1MB
-  },
-  fileFilter: (req, file, cb) => {
-    if (file.fieldname === 'avatar') {
-      if (
-        file.mimetype === 'image/png' ||
-        file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg'
-      ) {
-        cb(null, true);
-      } else {
-        cb(new Error('only .jpg, .png or .jpeg format allowed...!!!'));
-      }
-    } else if (file.fieldname === 'doc') {
-        if (file.mimetype === 'application/pdf') {
-          cb(null, true);
-        } else {
-          cb(new Error('Only .pdf format allowed'))
-        }
-    } else {
-      cb(new Error('There was an unknown error...!!!'));
-    }
-  },
-})
-
+// express app initialization
 const app = express();
+app.use(express.json());
 
-// application route
-app.post('/', upload.fields([
-  {name: 'avatar', maxCount: 1},
-  {name: 'doc', maxCount: 1}
-]), (req, res) => {
-  console.log(req.files);
-  res.send('Hello world');
-});
+// application routes
 
 // default error handler
-app.use((err, req, res, next) => {
-  if (err) {
-    if (err instanceof multer.MulterError) {
-      res.status(500).send('There was an upload error');
-    } else {
-      res.status(500).send(err.message);
-    }
-  } else {
-    res.send('success');
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
   }
-});
+  res.status(500).json({error: err});
+}
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
