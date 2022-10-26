aa3f49c878671eb05482c1977605aea06acd2817
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-26 11:16:45 +0600

    how to upload singl or multiple file

--- index.js
+++ index.js
@@ -1,56 +1,23 @@
 const express = require('express');
-const fs = require('fs');
+const multer = require('multer');
+
+// File upload folder
+const UPLOADS_FOLDER = './uploads/';
+
+// prepare the final multer upload object
+var upload = multer({
+  dest: UPLOADS_FOLDER
+})
+
 const app = express();
 
-// app.get('/', (req, res, next) => {
-//   fs.readFile('/file-does-not-exist', (err, data) => {
-//     if (err) {
-//       next(err);
-//     } else {
-//       res.send(data);
-//     }
-//   });
-// });
-
-// app.get('/', (req, res, next) => {
-//   setTimeout(function () {
-//     try {
-//       console.log(a);
-//     } catch (err) {
-//       next(err);
-//     }
-//   }, 100);
-// })
-
-app.get('/', [
-  (req, res, next) => {
-    fs.readFile('/file-doesnt-exist', 'utf-8', (err, data) => {
-      console.log(data);
-      next(err);
-    });
-  },
-  (req, res, next) => {
-    console.log(data.property);
-  }
-])
-
-app.use((req, res, next) => {
-  console.log('I am not called');
-  next();
-})
-
-// custom error handling
-app.use((err, req, res, next) => {
-  if (res.headersSend) {
-    next('There was a problem');
-  } else {
-    if (err.message) {
-      res.status(500).send(err.message);
-    } else {
-      res.send('There wan an error');
-    }
-  }
-})
+// application route
+app.post('/', upload.fields([
+  {name: 'avatar', maxCount: 1},
+  {name: 'gallery', maxCount: 2},
+]), (req, res) => {
+  res.send('Hello world');
+});
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
