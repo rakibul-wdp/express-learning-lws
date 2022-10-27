335298a159e65baaf3fbd740738d90c1ea23cf58
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-27 07:32:54 +0600

    learned how to insert multiple data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -32,7 +32,17 @@ router.post('/', async (req, res) => {
 
 // POST MULTIPLE TODO
 router.post('/all', async (req, res) => {
-  
+  await Todo.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: 'There was a server side error'
+      })
+    } else {
+      res.status(200).json({
+        message: 'Todos were inserted successfully'
+      })
+    }
+  })
 });
 
 // PUT TODO
