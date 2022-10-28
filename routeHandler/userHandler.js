e17e87ee7da6e6924954b06ee8abf713cc6f187c
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-28 11:01:09 +0600

    signup process

--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const userSchema = require('../schemas/userSchema');
 const User = new mongoose.model('User', userSchema);
 
 // SIGNUP
-router.get('/', (req, res) => {
-  
+router.post('/signup', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(200).json({
+      message: 'Signup was successful'
+    })
+  } catch {
+    res.status(500).json({
+      error: 'Signup failed'
+    })
+  }
 });
 
 module.exports = router;
