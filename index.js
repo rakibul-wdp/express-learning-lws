0396139dfa5da84b54d71b113c2b7b69e56832e7
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-28 11:16:38 +0600

    install jwt and dotenv and create token

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const todoHandler = require('./routeHandler/todoHandler');
 const userHandler = require('./routeHandler/userHandler');
 
 // express app initialization
 const app = express();
+dotenv.config();
 app.use(express.json());
 
 // database connection with mongoose
--- routeHandler/userHandler.js
+++ routeHandler/userHandler.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const userSchema = require('../schemas/userSchema');
 const User = new mongoose.model('User', userSchema);
@@ -22,6 +23,30 @@ router.post('/signup', async (req, res) => {
     res.status(500).json({
       error: 'Signup failed'
     })
+  }
+});
+
+// LOGIN
+router.post('/login', async(req, res) => {
+  const user = await User.find({username: req.body.username});
+  if (user && user.length > 0) {
+    const isValidPassword = await bcrypt.compare(req.body.password, user[0].password);
+
+    if (isValidPassword) {
+      // generate token
+      const token = jwt.sign({
+        username: user[0].username,
+        userId: user[0]._id,
+      }, process.env.JWT_SECRET);
+    } else {
+      res.status(401).json({
+        'error': 'Authentication failed'
+      });
+    }
+  } else {
+    res.status(401).json({
+      'error': 'Authentication failed'
+    });
   }
 });
 
