f6c86308717835fa5497686d67612fde6409d1db
rakibul-wdp <rakibul.wdp@gmail.com>
2022-10-28 11:38:02 +0600

    create jwt function

---
+++ middlewares/checkLogin.js
@@ -0,0 +1,15 @@
+const checkLogin = (req, res, next) => {
+  const {authorization} = req.headers;
+  try {
+    const token = authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT-SECRET);
+    const {username, userId} = decoded;
+    req.username = username;
+    req.userId = userId;
+    next();
+  } catch {
+    next('Authentication failure!')
+  }
+};
+
+module.exports = checkLogin;
